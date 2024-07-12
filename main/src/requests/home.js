5a785d1613b6547cdb9ee2bfc2461c43d1cc2e19
V Manin <victormanin151@gmail.com>
2024-07-12 15:42:08 +0300

    imp gifdetails

--- main/src/requests/home.js
+++ main/src/requests/home.js
@@ -1,17 +1,19 @@
+import { addImageListener, displayGifDetails } from "./gifdetails.js";
+ 
 document.addEventListener('DOMContentLoaded', () => {
     const homeButton = document.getElementById('home-button');
     const gifContainer = document.getElementById('gifContainer');
-
+ 
     const apiKey = 'EOtMOyfgb7swQVKzeUpvw3IDeR1XT48M';
-
+ 
     homeButton.addEventListener('click', () => {
-      fetchRandomGifs(5); 
+      fetchRandomGifs(5);
     });
-
+ 
     async function fetchRandomGifs(count) {
-      gifContainer.innerHTML = ''; 
+      gifContainer.innerHTML = '';
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
-
+ 
       try {
         const gifPromises = Array.from({ length: count }, () => fetch(url).then(response => {
           if (!response.ok) {
@@ -19,14 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
           }
           return response.json();
         }));
-
+ 
         const gifs = await Promise.all(gifPromises);
         gifs.forEach(data => {
           const gif = data.data;
           const img = document.createElement('img');
           img.src = gif.images.fixed_height.url;
+          img.dataset.gifId = gif.id;
           gifContainer.appendChild(img);
         });
+        addImageListener();
       } catch (error) {
         console.error('Error fetching GIFs:', error);
         gifContainer.textContent = 'Failed to load GIFs.';
