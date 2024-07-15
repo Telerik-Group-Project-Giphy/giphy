577054abf9ed3f1e74e9835bbc0be4156491de12
V Manin <victormanin151@gmail.com>
2024-07-15 10:40:42 +0300

    fixed renderContainer

--- main/src/views/renderContainers.js
+++ main/src/views/renderContainers.js
@@ -1,26 +1,61 @@
 import { addImageListener } from "../events/gifdetails.js";
 import { isFavorite, toggleFavorite } from "../data/manageFavorites.js";
 
+export function createFavoriteButton(gifId) {
+    const addToFavoritesButton = document.createElement('button');
+    addToFavoritesButton.className = 'favorite-button';
+    addToFavoritesButton.textContent = isFavorite(gifId) ? 'Remove from Favorites' : 'Add to Favorites';
+
+    addToFavoritesButton.addEventListener('click', () => {
+        toggleFavorite(gifId);
+        addToFavoritesButton.textContent = isFavorite(gifId) ? 'Remove from Favorites' : 'Add to Favorites';
+    });
+
+    return addToFavoritesButton;
+};
+
+export function createGifElement(gif, gifContainer, img) {
+    const gifWrapper = document.createElement('div');
+    gifWrapper.appendChild(img);
+    gifWrapper.appendChild(createFavoriteButton(gif.id));
+    gifContainer.appendChild(gifWrapper);
+};
+
+export function createGifImage(gif) {
+    const img = document.createElement('img');
+    img.src = gif.images.fixed_height.url;
+    img.dataset.gifId = gif.id;
+    return img;
+};
 
 export const transferToHTML = (data) => {
     gifContainer.innerHTML = '';
     data.data.forEach(gif => {
-        const img = document.createElement('img');
-        img.src = gif.images.fixed_height.url;
-        img.dataset.gifId = gif.id;
-
-        const addToFavoritesButton = document.createElement('button');
-        addToFavoritesButton.className = 'favorite-button';
-        addToFavoritesButton.textContent = isFavorite(gif.id) ? 'Remove from Favorites' : 'Add to Favorites';
-        addToFavoritesButton.addEventListener('click', () => {
-            toggleFavorite(gif.id);
-            addToFavoritesButton.textContent = isFavorite(gif.id) ? 'Remove from Favorites' : 'Add to Favorites';
-        });
-        const gifWrapper = document.createElement('div');
-        gifWrapper.appendChild(img);
-        gifWrapper.appendChild(addToFavoritesButton);
-        gifContainer.appendChild(gifWrapper);
-
-    })
+        const img = createGifImage(gif);
+        createGifElement(gif, gifContainer, img);
+    });
     addImageListener();
 };
+
+
+// export const transferToHTML = (data) => {
+//     gifContainer.innerHTML = '';
+//     data.data.forEach(gif => {
+        // const img = document.createElement('img');
+        // img.src = gif.images.fixed_height.url;
+        // img.dataset.gifId = gif.id;
+
+        // const addToFavoritesButton = document.createElement('button');
+        // addToFavoritesButton.className = 'favorite-button';
+        // addToFavoritesButton.textContent = isFavorite(gif.id) ? 'Remove from Favorites' : 'Add to Favorites';
+        // addToFavoritesButton.addEventListener('click', () => {
+        //     toggleFavorite(gif.id);
+        //     addToFavoritesButton.textContent = isFavorite(gif.id) ? 'Remove from Favorites' : 'Add to Favorites';
+        // });
+        // const gifWrapper = document.createElement('div');
+        // gifWrapper.appendChild(img);
+        // gifWrapper.appendChild(addToFavoritesButton);
+        // gifContainer.appendChild(gifWrapper);
+//     })
+//     addImageListener();
+// };
