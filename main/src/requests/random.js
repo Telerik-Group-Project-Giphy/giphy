6aa7ac685798c2389feb1d2152de5784948f9ddd
V Manin <victormanin151@gmail.com>
2024-07-16 11:40:59 +0300

    added JSDoc for random.js

--- main/src/requests/random.js
+++ main/src/requests/random.js
@@ -2,6 +2,13 @@ import { addImageListener } from "../events/gifdetails.js";
 import { apiKey } from "../common/common.js";
 import { createGifElement, createGifImage } from "../views/renderContainers.js";
 
+/**
+ * Fetches a random GIF from the Giphy API and displays it in the gifContainer.
+ * If the fetch operation fails, an error message is displayed in the gifContainer.
+ * 
+ * @async
+ * @function fetchRandomGif
+ */
 export async function fetchRandomGif() {
   gifContainer.innerHTML = '';
   const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
